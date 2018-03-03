86b610610f31d458a96bcaf8eb89e4be5761e592
ThangEthan <thangvu201098@gmail.com>
2018-03-03 04:04:41 -0800

    adding thang is here

---
+++ cpp-lang-console-project-11/main.cpp
@@ -0,0 +1,24 @@
+//=============================================================================
+//	APPLICATION:	cpp_lang_console_project_11
+//	MODULE:			main
+//	PURPOSE:
+//	AUTHOR(S):		User
+//
+//	3/3/2018 3:53:03 AM	Created.
+//=============================================================================
+#include <iostream>
+using namespace std;
+//-----------------------------------------------------------------------------
+//	THE MAIN ENTRY POINT TO THE PROGRAM.
+//
+//	input:	argc	The number of arguments of the programme.
+//			argv	The vector of arguments of the programme.
+//	return:	The code of the reason the process was terminated for.
+//			The value defaults to zero.
+//-----------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+	cout << "My program in C++ Language." << endl;
+
+	return 0;
+}
